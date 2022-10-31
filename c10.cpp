8e30d9ae4115e36ab6a3408fc3608d93ec730ede
Yuvarajnavuru <112234788+Yuvarajnavuru@users.noreply.github.com>
2022-10-31 14:58:15 +0530

    Add files via upload

---
+++ c10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cout<<"ENTER THE NUMBER:";
+	cin>>n;
+	if(n%2==0)
+	cout<<n<<" is even";
+	else
+	cout<<n<<" is odd";
+	
+}
---
+++ c19.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,j,n;
+cout<<"enter the number of rows => ";
+cin>>n;
+for(j=0;j=n;j--)
+{
+for(i=0;i=n;i--)
+{
+cout<<"*";
+}
+cout<<"\n=n";
+}
+}
+
